8e6d7fb1a67711b46a3e83f409368390d225f876
miltenkot <miltenkot@gmail.com>
2018-09-07 17:08:33 +0200

    Initial Commit

---
+++ SlenderMan ARTests/SlenderMan_ARTests.swift
@@ -0,0 +1,34 @@
+//
+//  SlenderMan_ARTests.swift
+//  SlenderMan ARTests
+//
+//  Created by Bartek Lanczyk on 07.09.2018.
+//  Copyright © 2018 miltenkot. All rights reserved.
+//
+
+import XCTest
+@testable import SlenderMan_AR
+
+class SlenderMan_ARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
